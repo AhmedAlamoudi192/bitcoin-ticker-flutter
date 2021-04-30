7d32017450c7f327e0e0a1421ea05a5268964f80
Ahmed Alamoudi <hamode192@gmail.com>
2021-04-30 22:12:09 +0300

    challenge fully done

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,6 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +31,20 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+class CoinData {
+  CoinData();
+
+  Future getCoinData(String currency,String type) async {
+    Uri coinApibtc = Uri.parse(
+        "https://rest.coinapi.io/v1/exchangerate/$type/$currency?apikey=FBE66C1C-023C-40A4-927B-8854ABC98623");
+    http.Response btcResponse = await http.get(coinApibtc);
+    if (btcResponse.statusCode != 200) {
+      print('somthing went wrong');
+      print(currency);
+      print(btcResponse.statusCode);
+      return;
+    }
+    print('all good');
+    return jsonDecode(btcResponse.body)['rate'];
+  }
+}
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,4 +1,7 @@
+import 'coin_data.dart';
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -6,46 +9,118 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String selectedCurrency = 'USD';
+  List<String> price = ['?', '?', '?'];
+
+  Widget iosPicker() {
+    List<Widget> pickerItems = [
+      for (String currency in currenciesList) Text(currency)
+    ];
+
+    return CupertinoPicker(
+      itemExtent: 32.0,
+      onSelectedItemChanged: (int value) {
+        print(value);
+      },
+      children: pickerItems,
+    );
+  }
+
+  Widget androidPicker() {
+    List<DropdownMenuItem<String>> dropdownMenuItems = [
+      for (String currency in currenciesList)
+        DropdownMenuItem(
+          child: Text(currency),
+          value: currency,
+        )
+    ];
+
+    return DropdownButton<String>(
+      value: selectedCurrency,
+      items: dropdownMenuItems,
+      onChanged: (value) {
+        setState(
+          () {
+            selectedCurrency = value;
+            for (String type in cryptoList) {
+              updatenumber(type);
+            }
+          },
+        );
+      },
+    );
+  }
+
+  void updatenumber(String type) async {
+    double number = await CoinData().getCoinData(selectedCurrency, type);
+    setState(() {
+      (type == "BTC")
+          ? price[0] = number.toStringAsFixed(0)
+          : (type == "ETH")
+              ? price[1] = number.toStringAsFixed(0)
+              : price[2] = number.toStringAsFixed(0);
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    for (String type in cryptoList) {
+      updatenumber(type);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('🤑 Coin Ticker'),
+        title: Text('Coin Ticker'),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Padding(
-            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.lightBlueAccent,
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-                child: Text(
-                  '1 BTC = ? USD',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              createCard(price[0], selectedCurrency, cryptoList[0]),
+              createCard(price[1], selectedCurrency, cryptoList[1]),
+              createCard(price[2], selectedCurrency, cryptoList[2]),
+            ],
           ),
           Container(
             height: 150.0,
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: Platform.isIOS ? iosPicker() : androidPicker(),
           ),
         ],
       ),
     );
   }
+}
+
+Widget createCard(String price, String currency, String type) {
+  return Padding(
+    padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+    child: Card(
+      color: Colors.lightBlueAccent,
+      elevation: 5.0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+        child: Text(
+          '1 $type = $price $currency',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 20.0,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    ),
+  );
 }
